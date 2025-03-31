790320c8dc8e822184b6904ba9cad2c82dddfd7a
Bernhard Rainer <bernhard.rainer@outlook.at>
2025-03-31 23:57:14 +0200

    Updated Vulkan dependencies (#9)

--- deps/vk-bootstrap.cmake
+++ deps/vk-bootstrap.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   vk-bootstrap
   GIT_REPOSITORY https://github.com/charles-lunarg/vk-bootstrap.git
-  GIT_TAG v1.3.302
+  GIT_TAG v1.4.311
 )
 
 set(VK_BOOTSTRAP_TEST OFF CACHE BOOL "" FORCE)
